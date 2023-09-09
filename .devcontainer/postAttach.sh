57f93dce237c21e3653608041476c03ba66eccd7
Kenjiro Kuge <kugetyan0211@gmail.com>
2023-09-09 12:48:17 +0000

    Modified Docker files and Dev Containers config

---
+++ .devcontainer/postAttach.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd `dirname $0`
+cd ..
+sudo chown -R devcontainer ~
+
+# install go development kit
+go install golang.org/x/tools/gopls@latest
+go install golang.org/x/tools/cmd/godoc@latest
+go install golang.org/x/lint/golint@latest
+go install github.com/rogpeppe/godef@latest
+go install github.com/go-delve/delve/cmd/dlv@master
+go install github.com/haya14busa/goplay/cmd/goplay@v1.0.0
+go install github.com/fatih/gomodifytags@v1.16.0
+go install github.com/josharian/impl@latest
+go install github.com/cweill/gotests/gotests@latest
+go install github.com/ramya-rao-a/go-outline@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
